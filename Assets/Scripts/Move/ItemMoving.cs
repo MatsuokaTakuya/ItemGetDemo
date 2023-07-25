d32ea154c98f5d47d1313985dd26c44b7fa8aca2
DESKTOP-0V0B8D7\matsuoka <matsuokagame@gmail.com>
2023-07-26 06:49:07 +0900

    持つ状態のアニメーション作成 アイテムのムーブメント追加

---
+++ Assets/Scripts/Move/ItemMoving.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ItemMoving : MonoBehaviour
+{
+    [Header("ã‰º•‚ğİ’è")]
+    public float sinOffset = 3f;
+    [Header("‰ñ“]‘¬“x‚ğİ’è")]
+    public float rotateSpeed = 1f;
+
+    private float offsetY;
+
+    private void Start()
+    {
+        offsetY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        var posY = Mathf.Sin(Time.time) / sinOffset;
+        Vector3 updateVec = transform.position;
+        updateVec.y = offsetY + posY;
+        transform.position = updateVec;
+
+        transform.Rotate(0f, rotateSpeed, 0f);
+    }
+}
